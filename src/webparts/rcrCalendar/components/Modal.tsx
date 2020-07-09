be46b1e1a67e07d4c92223daa91a44e124ea9924
Filat Astakhov <astakhovfilat@gmail.com>
2020-07-09 16:37:06 +0500

    Доработка стилей модального окна

--- src/webparts/rcrCalendar/components/Modal.tsx
+++ src/webparts/rcrCalendar/components/Modal.tsx
@@ -11,8 +11,8 @@ const Modal: React.FunctionComponent<IModalProps> = (props) => {
     return (
         <div className={styles.bg}>
             <div className={styles.border}>
-                <div className={styles.title}>{props.title}</div>
                 <div className={styles.close} onClick={props.closeModalFn}></div>
+                <div className={styles.title}>{props.title}</div>
                 <div className={styles.window}>
                     {props.children}
                 </div>
