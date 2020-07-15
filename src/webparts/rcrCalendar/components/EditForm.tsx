9181c06004c9ced51900b6a5313f0c15bdd1f347
Filat Astakhov <astakhovfilat@gmail.com>
2020-07-15 10:28:58 +0500

    Добавлены поля в форму редактирования

--- src/webparts/rcrCalendar/components/EditForm.tsx
+++ src/webparts/rcrCalendar/components/EditForm.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
-import { Form, Input, Checkbox, Button } from 'antd';
+import { Form, Input, Checkbox, Button, Select, DatePicker, TimePicker } from 'antd';
+import * as moment from "moment";
+
+
 
 const EditForm = () => {
+
+    const [form] = Form.useForm();
+
     const layout = {
         labelCol: { span: 8 },
         wrapperCol: { span: 16 },
@@ -19,6 +25,20 @@ const EditForm = () => {
         console.log('Failed:', errorInfo);
     };
 
+    const onGenderChange = value => {
+        switch (value) {
+            case "male":
+                form.setFieldsValue({ note: "Hi, man!" });
+                return;
+            case "female":
+                form.setFieldsValue({ note: "Hi, lady!" });
+                return;
+            case "other":
+                form.setFieldsValue({ note: "Hi there!" });
+                return;
+        }
+    };
+
     return (
         <Form
             {...layout}
@@ -28,19 +48,37 @@ const EditForm = () => {
             onFinishFailed={onFinishFailed}
         >
             <Form.Item
-                label="Username"
-                name="username"
-                rules={[{ required: true, message: 'Please input your username!' }]}
+                label="Название"
+                name="eventName"
+                rules={[{ required: true, message: 'Пожалуйста, введите название события.' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item name="gender" label="Категория события" rules={[{ required: true }]}>
+                <Select
+                    placeholder=""
+                    // onChange={onGenderChange}
+                    allowClear
+                >
+                    <Select.Option value="external">Внешнее обучение</Select.Option>
+                    <Select.Option value="other">Прочие события</Select.Option>
+                </Select>
+            </Form.Item>
+
+            <Form.Item
+                label="Расположение"
+                name="location"
             >
                 <Input />
             </Form.Item>
 
             <Form.Item
-                label="Password"
-                name="password"
-                rules={[{ required: true, message: 'Please input your password!' }]}
+                label="Время начала"
+                name="eventName"
+                rules={[{ required: true }]}
             >
-                <Input.Password />
+                <DatePicker />
             </Form.Item>
 
             <Form.Item {...tailLayout} name="remember" valuePropName="checked">
--- src/webparts/rcrCalendar/components/RcrCalendar.tsx
+++ src/webparts/rcrCalendar/components/RcrCalendar.tsx
@@ -7,6 +7,7 @@ import Content from './Content';
 import Calendar from './Calendar';
 import EventCard from './EventCard';
 import "@pnp/polyfill-ie11";
+import EditForm from './EditForm';
 
 export default class RcrCalendar extends React.Component<IRcrCalendarProps, {}> {
   public render(): React.ReactElement<IRcrCalendarProps> {
@@ -14,6 +15,7 @@ export default class RcrCalendar extends React.Component<IRcrCalendarProps, {}> 
       <div className={styles.rcrCalendar}>
         <div className={styles.header}>{escape(this.props.description)}</div>
         <div className={styles.app}>
+          <EditForm></EditForm>
           <Content>
             <EventCard></EventCard>
             <EventCard></EventCard>
